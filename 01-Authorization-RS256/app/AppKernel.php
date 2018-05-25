71090b987a8d9c9159b901a3be59635eb90fe5d6
Alexis Luque <aluque@indexlabs.com.uy>
2018-05-25 17:39:51 -0300

    Add CORS support

--- 01-Authorization-RS256/app/AppKernel.php
+++ 01-Authorization-RS256/app/AppKernel.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Bundle\WebServerBundle\WebServerBundle;
 
 class AppKernel extends Kernel
 {
@@ -17,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new Auth0\JWTAuthBundle\JWTAuthBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
 
         ];
 
@@ -25,6 +27,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new WebServerBundle();
         }
 
         return $bundles;
